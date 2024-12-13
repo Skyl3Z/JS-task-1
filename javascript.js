6ec660ed9b8691ec81ab1354d4d3da702579228c
Skyl3Z <sabagogichaishvili15@gmail.com>
2024-12-13 21:34:33 +0400

    js task 1

---
+++ javascript.js
@@ -0,0 +1,74 @@
+// let saba= 10 ;
+// saba= 20 ;
+// console.log(saba);
+
+// let messi= 7 ; 
+// messi= 15 ;
+// console.log(messi);
+
+// const gogich= 30 ;
+// console.log(gogich);
+
+// let x=16 ;
+// let y=4 ;
+// console.log(x+y) ;
+
+// let x=9 ;
+// let y=5 ;
+// console.log(x-y) ;
+
+// let x=5 ;
+// let y= 25 ;
+// console.log(x*y) ;
+
+// let x= 36 ;
+// let y= 6 ;
+// console.log(x/y) ;
+
+// let x= 45 ;
+// let y= 5 ;
+// let z= 8 ;
+// console.log(x-y*z) ;
+
+// let x= 10 ; 
+// let y= 15 ; 
+// let z=5 ;
+// let w=8 ; 
+// let t= 4 ;
+// console.log(w/t+y/z*x) ;
+
+// let x= 50 ;
+// console.log(x/(5+5)) ;
+
+// let x= 12 ;
+// x++
+// x++
+// console.log(x)
+
+// let x=20 ;
+// x--
+// console.log(x) ;
+
+// let x= 10 ;
+// x+=20
+// console.log(x) ;
+
+// let x= 12 ;
+// x-= 5 ;
+// console.log(x) ;
+
+// let x= 5 ;
+// x*= 4 ;
+// console.log(x) ;
+
+// let x= 21 ;
+// x/= 7 ;
+// console.log(x) ;
+
+// let x= 10 ;
+// let y= 7 ; 
+// console.log(x<y) ;
+
+let x= 20 ;
+let y= 14 ;
+console.log(x>y) ;
